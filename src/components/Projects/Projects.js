9ed5b8fe8c22d5265fc4c6b7a5e335a5d1b062dc
captainPrime <taiwooyindamola732@gmail.com>
2023-11-25 18:02:29 +0100

    fix: new updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,6 @@ function Projects() {
               isBlog={false}
               title="Sofri Pro"
               description="Financial services accessible wherever you are and whenever you want: mobile banking transactions, reliable and instant debit card, and the most effective investment tools "
-              ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://sofrisofri.com/"
             />
           </Col>
@@ -38,7 +37,6 @@ function Projects() {
               isBlog={false}
               title="Niptrex"
               description="Niptrex is a start-up talent-hiring agency that specializes in finding and recruiting for job positions on behalf of employers. These agencies work with companies of various sizes and industries to help them fill their job vacancies with qualified and suitable candidates."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
             />
           </Col>
 
@@ -52,7 +50,6 @@ function Projects() {
 They have the finest professionally trained and vetted independent riders dedicated to delivering packages using bicycles, motorbikes, cars, and minivans.
 
 They organize our Hoppers to prioritize speed and safety to your convenience for same-day or scheduled delivery."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://play.google.com/store/apps/details?id=ng.quickbunny.app"
             />
           </Col>
@@ -62,7 +59,6 @@ They organize our Hoppers to prioritize speed and safety to your convenience for
               isBlog={false}
               title="Sabinus App"
               description="For lovers of the African culture, Sabinus is the first-word game that entertains and sharpens your pidgin language skills."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               demoLink="https://play.google.com/store/apps/details?id=io.sabinus.app"
             />
           </Col>
@@ -73,7 +69,6 @@ They organize our Hoppers to prioritize speed and safety to your convenience for
               isBlog={false}
               title="Afinju"
               description="Afinju is an art gallery platform. From contemporary masterpieces to avant-garde installations, they curate a collection that captivates and inspires."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
               demoLink="https://afinju.co.uk/"
             />
           </Col>
@@ -86,7 +81,6 @@ They organize our Hoppers to prioritize speed and safety to your convenience for
               description="Pikinology are designing and creating culturally relevant clothes that speaks to the history and culture of every African child and allows them to be proud of who they are and where they are from
 
 Through each button, fabric, trim, lining, illustrations, graphics et al, we want to tell our own stories."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
               demoLink="https://pikinology.com/"
             />
           </Col>
